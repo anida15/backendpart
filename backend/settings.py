9ca1444a18bbe90066d2bb3422f6afabef0834fa
anidakim <61341735+anida15@users.noreply.github.com>
2022-03-17 17:32:38 +0300

    second

--- backend/settings.py
+++ backend/settings.py
@@ -88,13 +88,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myhostel',
+        'USER': 'anida',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 
 AUTH_USER_MODEL ="user.CustomUser" 
